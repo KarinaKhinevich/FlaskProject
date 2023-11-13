3ae6b6471a940da60e7baa788dbbec47ba3bb264
KarinaKhinevich <karina.khinevich@gmail.com>
2023-11-13 21:44:46 +0100

    databases

--- project.py
+++ project.py
@@ -1,4 +1,8 @@
 from app import app
+from app.models import User, Post
 
 
-
+#создаем контекст оболочки для flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
